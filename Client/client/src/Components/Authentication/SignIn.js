d8ae9f6ec4dbaea709b13c7e2e9cfb5b8b08d09e
Nikhil-Gupta-prog <nikhilgupta122000@gmail.com>
2021-09-05 05:07:06 +0530

    Login Page update

--- Client/client/src/Components/Authentication/SignIn.js
+++ Client/client/src/Components/Authentication/SignIn.js
@@ -1,79 +1,72 @@
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./SignIn.css";
-
-
-
 
 const SignIn = () => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
-  const [emailIsTouched, setEmailIsTouched] = useState(false)
-  const [passwordIsTouched, setPasswordIsTouched] = useState(false)
+  const [emailIsTouched, setEmailIsTouched] = useState(false);
+  const [passwordIsTouched, setPasswordIsTouched] = useState(false);
 
-
-  const emailChangeHandler = (e) =>{
+  const emailChangeHandler = (e) => {
     setEnteredEmail(e.target.value);
-  }
-  const passwordChangeHandler = (e)=>{
+  };
+  const passwordChangeHandler = (e) => {
     setEnteredPassword(e.target.value);
-  }
-  const emailInputBlurHandler = () =>{
+  };
+  const emailInputBlurHandler = () => {
     setEmailIsTouched(true);
-  }
-  const passwordInputBlurHandler = () =>{
+  };
+  const passwordInputBlurHandler = () => {
     setPasswordIsTouched(true);
-  }
+  };
 
-  const emailIsValid = enteredEmail.includes('@')  ;
-  const passwordIsValid = enteredPassword.length >=4;
+  const emailIsValid = enteredEmail.includes("@");
+  const passwordIsValid = enteredPassword.length >= 4;
 
-
- 
-
-  const submitHandler = (e) =>{
+  const submitHandler = (e) => {
     e.preventDefault();
-   
-    console.log(enteredEmail,enteredPassword);
-    setEnteredEmail('');
-    setEnteredPassword('');
-  }
+
+    console.log(enteredEmail, enteredPassword);
+    setEnteredEmail("");
+    setEnteredPassword("");
+  };
 
   return (
     <div className="signIn_parent">
       <div className="signIn_child">
-        <div></div>
-        <div>
-        <form onSubmit={submitHandler}>
-          <p className="signIn_heading">Login Here</p>
-          <div className="signIn_InputBlock">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              value={enteredEmail}
-              onChange={emailChangeHandler}
-              onBlur={emailInputBlurHandler}
-
-            />
-            {!emailIsValid && <p>Please enter your email</p>}
-           
-          </div>
-          <div className="signIn_InputBlock">
-            <label htmlFor="password">Passowrd</label>
-            <input
-              id="password"
-              type="password"
-              value={enteredPassword}
-              onChange={passwordChangeHandler}
-              onBlur={passwordInputBlurHandler}
-            />
-            {!passwordIsValid && <p>Please enter your password</p>}
-
-          </div>
-          <div className="signIn_action">
-            <button>Login</button>
-          </div>
-        </form>
+        <div className="signIn_child_part1">
+          <p className="signIn_child_part1_heading"> WELCOME BACK!</p>
+          <p className="signIn_child_part1_subheading">To keep connected with us please login with Us</p>
+        </div>
+        <div className="signIn_child_part2">
+          <form onSubmit={submitHandler}>
+            <p className="signIn_heading">Login Here</p>
+            <div className="signIn_InputBlock">
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="email"
+                value={enteredEmail}
+                onChange={emailChangeHandler}
+                onBlur={emailInputBlurHandler}
+              />
+              {!emailIsValid && <p>Please enter your email</p>}
+            </div>
+            <div className="signIn_InputBlock">
+              <label htmlFor="password">Passowrd</label>
+              <input
+                id="password"
+                type="password"
+                value={enteredPassword}
+                onChange={passwordChangeHandler}
+                onBlur={passwordInputBlurHandler}
+              />
+              {!passwordIsValid && <p>Please enter your password</p>}
+            </div>
+            <div className="signIn_action">
+              <button>Login</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
