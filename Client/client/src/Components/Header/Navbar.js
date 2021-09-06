38a6df9dd05fef91b6baadfd6c50e1b820b823bf
Nikhil-Gupta-prog <nikhilgupta122000@gmail.com>
2021-09-07 01:42:39 +0530

    Home Page update with SVG,fonts and background

--- Client/client/src/Components/Header/Navbar.js
+++ Client/client/src/Components/Header/Navbar.js
@@ -1,56 +1,57 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import {Link} from "react-router-dom";
-import { FaBars } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 const Navbar = () => {
   const [navResponse, setNavResponse] = useState(false);
-  const navToggleHandler = () =>{
-    setNavResponse((prevState)=>!prevState);
-  }
+  const navToggleHandler = () => {
+    setNavResponse((prevState) => !prevState);
+  };
   return (
-   
     <nav className="parent_nav">
-      <div> <h1 className="nav_logo" >PlaceMentor</h1>
-
+      <div>
+        {" "}
+        <h1 className="nav_logo">PlaceMentor</h1>
       </div>
       <div>
-      <ul className={navResponse ?"parent_nav_list_mobileview":"parent_nav_list "}>
+        <ul
+          className={
+            navResponse ? "parent_nav_list_mobileview" : "parent_nav_list "
+          }
+        >
           <li>
-            <Link to ="/">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to ="about">About</Link>
+            <Link to="about">About</Link>
           </li>
           <li>
-            <Link to ="/signup">Sign Up</Link>
+            <Link to="/signup">Sign Up</Link>
           </li>
           <li>
-            <Link to ="/signin">Sign In</Link>
+            <Link to="/signin">Sign In</Link>
           </li>
 
           <li>
-            <Link to ="/form">Form</Link>
+            <Link to="/form">Form</Link>
           </li>
           <li>
-            <Link to ="/book">Books</Link>
+            <Link to="/book">Books</Link>
           </li>
           <li>
-            <Link to ="/profile">Profile</Link>
+            <Link to="/profile">Profile</Link>
           </li>
         </ul>
       </div>
       <div className="hamburger_icon" onClick={navToggleHandler}>
-      <IconContext.Provider
-      value={{ color: '#2d866d', size: '30px' }}
-    >
-      <div>
-        <FaBars />
+        <IconContext.Provider value={{ color: "#2d866d", size: "30px" }}>
+          <div>
+            <FaBars />
+          </div>
+        </IconContext.Provider>
       </div>
-    </IconContext.Provider>
-        </div>
- 
     </nav>
   );
 };
--- Client/client/src/Components/Home/Home.js
+++ Client/client/src/Components/Home/Home.js
@@ -1,29 +1,26 @@
 import React from "react";
 import "./Home.css";
-
-import Image from "../../Assests/home.png";
+import { Link } from "react-router-dom";
+import Image from "../../Assests/Home.svg";
 
 const Home = () => {
   return (
-    <div className="main_home">
-      <div className="home">
-        <div className="home_title">
-          <h4>Welcome to our</h4>
-          <p className="home_title_name">PLACEMENTOR</p>
+    <div className="home">
+      <div className="home_title">
+        <h3>Welcome to our</h3>
+        <p className="home_title_name">PLACEMENTOR</p>
 
-          <div className="button_block">
-            <button className="register_button button_slide slide_right">
-              Register
-            </button>
-            <div className="home_login_block">
-              <p>Already have an existing account&nbsp;</p>
-              <a href="#">Login</a>
-            </div>
-          </div>
+        <div className="button_block">
+          <Link to="/signup" className="home_register_button ">
+            SignUp
+          </Link>
+          <Link to="/signin" className="home_login_button">
+            Login{" "}
+          </Link>
         </div>
-        <div className="image_block">
-          <img src={Image} className="home_image" />
-        </div>
+      </div>
+      <div className="image_block">
+        <img src={Image} className="home_image" />
       </div>
     </div>
   );
